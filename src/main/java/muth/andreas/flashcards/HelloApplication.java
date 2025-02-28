5dd012510ff3021ab1734cffc9792fd72723f434
Andreas <a.muthenthaler@europagym.at>
2025-02-28 20:54:47 +0100

    Removed unnecessary code

--- src/main/java/muth/andreas/flashcards/HelloApplication.java
+++ src/main/java/muth/andreas/flashcards/HelloApplication.java
@@ -4,12 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.Connection;
 
 import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,26 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    private void getDB() {
-        try {
-            Connection connection = DriverManager.getConnection(System.getenv("DB_URL"), System.getenv("DB_User"), System.getenv("DB_PASSWORD"));
-            Statement statement = connection.createStatement();
-
-            statement.execute("CREATE TABLE IF NOT EXISTS flashcards (id INT PRIMARY KEY AUTO_INCREMENT, question VARCHAR(255), answer VARCHAR(255))");
-
-            statement.execute("SELECT * FROM flashcards");
-            statement.getResultSet().next();
-
-            String question = statement.getResultSet().getString("question");
-            System.out.println("next: " + question);
-
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static void main(String[] args) {
